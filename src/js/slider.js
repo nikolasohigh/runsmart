42674f58c3d78bba9365d6988a2dd338fd86a446
nikolasohigh <kolya.bondar.yt@gmail.com>
2022-09-01 22:35:25 +0300

    optimize js

--- src/js/slider.js
+++ src/js/slider.js
@@ -22,20 +22,18 @@ $(document).ready(function(){
           .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
       });
 
-    $('.catalog-item__link').each(function(i) {
-        $(this).on('click', function(e){
-            e.preventDefault();
-            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-        })
-    });
 
-    $('.catalog-item__back').each(function(i) {
-        $(this).on('click', function(e){
-            e.preventDefault();
-            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-        })
-    });
+      function toggleTab (item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e){
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        });
+      };
+
+    toggleTab ('.catalog-item__link');
+    toggleTab ('.catalog-item__back');
 
 });
