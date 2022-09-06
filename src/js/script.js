d80da1930f3fdb97189aae11661829b9b79f2e5d
nikolasohigh <kolya.bondar.yt@gmail.com>
2022-09-06 13:01:45 +0300

    smooth scroll

--- src/js/script.js
+++ src/js/script.js
@@ -108,7 +108,19 @@ $(document).ready(function(){
         })
         return false;
     });
-    
+    //Smooth scroll
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+    $("a[href^='#'").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
 });
 
     
